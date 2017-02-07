ce90f5024c89dab9a62d46e8447a23c8a443f80e
jeff(황용대) <jeff.yd@kakaocorp.com>
2017-02-07 11:23:36 +0900

    install mysql 5.7

---
+++ .travis.install-mysql-5.7.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo apt-get remove --purge "^mysql.*"
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /var/lib/mysql
+sudo rm -rf /var/log/mysql
+echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb
+sudo dpkg --install mysql-apt-config_0.8.1-1_all.deb

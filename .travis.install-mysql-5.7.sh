bf1b3d19bd9302719e0df149b4b57cccd5572752
jeff(황용대) <jeff.yd@kakaocorp.com>
2017-02-07 11:36:40 +0900

    add script on before_install section

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get remove --purge "^mysql.*"
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo rm -rf /var/lib/mysql
-sudo rm -rf /var/log/mysql
+sudo apt-get remove --purge "^mysql.*" && sudo apt-get autoremove && sudo apt-get autoclean
+sudo rm -rf /var/lib/mysql && sudo rm -rf /var/log/mysql
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb
 sudo dpkg --install mysql-apt-config_0.8.1-1_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
+sudo apt-get update -q && sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server

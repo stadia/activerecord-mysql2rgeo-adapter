c04250331807c51612bd29ed585c19b7a6aeb1f3
jeff.dean <jeff.dean@kakaomobility.com>
2025-03-04 13:48:45 +0900

    Update development dependencies and restore commented-out tests for spatial features

--- activerecord-mysql2rgeo-adapter.gemspec
+++ activerecord-mysql2rgeo-adapter.gemspec
@@ -25,7 +25,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "rgeo-activerecord", "~> 7.0.0"
   spec.add_dependency "rgeo", "~> 3.0"
 
-  spec.add_development_dependency "rake", "~> 12.0"
+  spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "minitest", "~> 5.4"
   spec.add_development_dependency "mocha", "~> 2.1"
   spec.add_development_dependency "appraisal", "~> 2.0"
--- test/tasks_test.rb
+++ test/tasks_test.rb
@@ -10,34 +10,34 @@ class TasksTest < ActiveSupport::TestCase
     assert(sql !~ /CREATE TABLE/)
   end
 
-  # def test_sql_dump
-  #   setup_database_tasks
-  #   connection.create_table(:spatial_test, force: true) do |t|
-  #     t.point "latlon", geographic: true
-  #     t.geometry "geo_col", srid: 4326
-  #     t.column "poly", :multi_polygon, srid: 4326
-  #   end
-  #   ActiveRecord::Tasks::DatabaseTasks.structure_dump(new_connection, tmp_sql_filename)
-  #   data = File.read(tmp_sql_filename)
-  #   assert_includes data, "`latlon` point"
-  #   assert_includes data, "`geo_col` geometry"
-  #   assert_includes data, "`poly` multipolygon"
-  # end
-  #
-  # def test_index_sql_dump
-  #   setup_database_tasks
-  #   connection.create_table(:spatial_test, force: true) do |t|
-  #     t.point "latlon", null: false, geographic: true
-  #     t.string "name"
-  #   end
-  #   connection.add_index :spatial_test, :latlon, type: :spatial
-  #   connection.add_index :spatial_test, :name, using: :btree
-  #   ActiveRecord::Tasks::DatabaseTasks.structure_dump(new_connection, tmp_sql_filename)
-  #   data = File.read(tmp_sql_filename)
-  #   assert_includes data, "`latlon` point"
-  #   assert_includes data, "SPATIAL KEY `index_spatial_test_on_latlon` (`latlon`)"
-  #   assert_includes data, "KEY `index_spatial_test_on_name` (`name`) USING BTREE"
-  # end
+  def test_sql_dump
+    setup_database_tasks
+    connection.create_table(:spatial_test, force: true) do |t|
+      t.point "latlon", geographic: true
+      t.geometry "geo_col", srid: 4326
+      t.column "poly", :multi_polygon, srid: 4326
+    end
+    ActiveRecord::Tasks::DatabaseTasks.structure_dump(new_connection, tmp_sql_filename)
+    data = File.read(tmp_sql_filename)
+    assert_includes data, "`latlon` point"
+    assert_includes data, "`geo_col` geometry"
+    assert_includes data, "`poly` multipolygon"
+  end
+
+  def test_index_sql_dump
+    setup_database_tasks
+    connection.create_table(:spatial_test, force: true) do |t|
+      t.point "latlon", null: false, geographic: true
+      t.string "name"
+    end
+    connection.add_index :spatial_test, :latlon, type: :spatial
+    connection.add_index :spatial_test, :name, using: :btree
+    ActiveRecord::Tasks::DatabaseTasks.structure_dump(new_connection, tmp_sql_filename)
+    data = File.read(tmp_sql_filename)
+    assert_includes data, "`latlon` point"
+    assert_includes data, "SPATIAL KEY `index_spatial_test_on_latlon` (`latlon`)"
+    assert_includes data, "KEY `index_spatial_test_on_name` (`name`) USING BTREE"
+  end
 
   def test_empty_schema_dump
     setup_database_tasks
@@ -58,8 +58,8 @@ class TasksTest < ActiveSupport::TestCase
       ActiveRecord::SchemaDumper.dump(connection, file)
     end
     data = File.read(tmp_sql_filename)
-    assert_includes data, "t.geometry \"object1\", limit: {:type=>\"geometry\", :srid=>#{connection.default_srid}"
-    assert_includes data, "t.geometry \"object2\", limit: {:type=>\"geometry\", :srid=>#{connection.default_srid}"
+    assert_includes data, "t.geometry \"object1\", limit: {type: \"geometry\", srid: #{connection.default_srid}"
+    assert_includes data, "t.geometry \"object2\", limit: {type: \"geometry\", srid: #{connection.default_srid}"
   end
 
   def test_basic_geography_schema_dump
@@ -72,13 +72,13 @@ class TasksTest < ActiveSupport::TestCase
       ActiveRecord::SchemaDumper.dump(connection, file)
     end
     data = File.read(tmp_sql_filename)
-    assert_includes data, %(t.geometry "latlon1", limit: {:type=>"point", :srid=>0})
+    assert_includes data, %(t.geometry "latlon1", limit: {type: "point", srid: 0})
     if connection.supports_index_sort_order?
       assert_includes(data,
-                      %(t.geometry "latlon2", limit: {:type=>"point", :srid=>4326}))
+                      %(t.geometry "latlon2", limit: {type: "point", srid: 4326}))
     else
       assert_includes(data,
-                      %(t.geometry "latlon2", limit: {:type=>"point", :srid=>0}))
+                      %(t.geometry "latlon2", limit: {type: "point", srid: 0}))
     end
   end
 
@@ -92,22 +92,21 @@ class TasksTest < ActiveSupport::TestCase
       ActiveRecord::SchemaDumper.dump(connection, file)
     end
     data = File.read(tmp_sql_filename)
-    puts data
-    assert_includes data, %(t.geometry "latlon", limit: {:type=>"point", :srid=>0}, null: false)
+    assert_includes data, %(t.geometry "latlon", limit: {type: "point", srid: 0}, null: false)
     assert_includes data, %(t.index ["latlon"], name: "index_spatial_test_on_latlon", type: :spatial)
   end
 
-  # def test_add_index_with_no_options
-  #   setup_database_tasks
-  #   connection.create_table(:test, force: true) do |t|
-  #     t.string "name"
-  #   end
-  #   connection.add_index :test, :name
-  #   ActiveRecord::Tasks::DatabaseTasks.structure_dump(new_connection, tmp_sql_filename)
-  #   data = File.read(tmp_sql_filename)
-  #   assert_includes data, "KEY `index_test_on_name` (`name`)"
-  # end
-  #
+  def test_add_index_with_no_options
+    setup_database_tasks
+    connection.create_table(:test, force: true) do |t|
+      t.string "name"
+    end
+    connection.add_index :test, :name
+    ActiveRecord::Tasks::DatabaseTasks.structure_dump(new_connection, tmp_sql_filename)
+    data = File.read(tmp_sql_filename)
+    assert_includes data, "KEY `index_test_on_name` (`name`)"
+  end
+
   # def test_add_index_via_references
   #   setup_database_tasks
   #   connection.create_table(:cats, force: true)
